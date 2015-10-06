251acdd95059b833de2a7612c7341b6860d4e8c1
Ilya <MrCarlione@yandex.ru>
2015-10-06 08:29:15 +0300

    Commited new project
    New project

---
+++ GP24MVC/Controllers/AboutController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GP24MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult ShemeWay()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+            return View();
+        }
+        public ActionResult Employeers()
+        {
+            return View();
+        }
+        public ActionResult MedCare()
+        {
+            return View();
+        }
+        public ActionResult Vacancy()
+        {
+            return View();
+        }
+    }
+}
---
+++ GP24MVC/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GP24MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
