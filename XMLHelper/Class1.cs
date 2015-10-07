e00672c6048025ecf23a554934876cd4d87c2854
Ilya <MrCarlione@yandex.ru>
2015-10-07 18:45:10 +0300

    Work update.
    Update sourse code.

---
+++ XMLHelper/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XMLHelper
+{
+    /// <summary>
+    /// This class is being serialized to .xml
+    /// </summary>
+    [Serializable]
+    [XmlRoot("doctors"), XmlType("doctors")]
+    public class XMLReader
+    {
+
+    }
+}
